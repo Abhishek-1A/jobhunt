c5de7bf8592bb3b67926e6945ba5eb2088588764
Abhishek-A <kua77663@gmail.com>
2024-02-23 11:37:42 +0530

    type mistake

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js"
 import errorHandler from "../middlewares/error.js";
 import { User } from "../models/userSchema.js";
-export const resister = catchAsyncError(async(req,res,next) =>{
+export const register = catchAsyncError(async(req,res,next) =>{
 const{name,email,phone, role,password} =req.body;
 if(!name || !email || phone || !role || !password) {
     return next(new EroorHandler("please fill full registration form!"));
