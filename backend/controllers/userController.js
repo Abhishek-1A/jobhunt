f6918f4877e63a04d86ac527bc8ff07981a42bfc
Abhishek-A <kua77663@gmail.com>
2024-02-23 11:35:02 +0530

    created user controller function in  which we handled whether is registred or not

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js"
+import errorHandler from "../middlewares/error.js";
+import { User } from "../models/userSchema.js";
+export const resister = catchAsyncError(async(req,res,next) =>{
+const{name,email,phone, role,password} =req.body;
+if(!name || !email || phone || !role || !password) {
+    return next(new EroorHandler("please fill full registration form!"));
+}
+const isEmail = await User.findOne({email});
+if(isEmail){
+    return next(new errorHandler("Email already exists!"));
+}
+
+const user = await User.create({
+    name,
+    email,
+    phone,
+    role,
+    password,
+});
+res.ststus(200).json({
+    success: true,
+    message: "User registered!",
+    user,
+})
+});
